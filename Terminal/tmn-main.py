6dcd64aeabd86649731fbbaac9025fb237636c09
ioriiod0 <ioriiod0@gmail.com>
2016-12-09 20:37:09 +0800

    Increase heartbeat packets and get mac info in Terminal

--- Terminal/tmn-main.py
+++ Terminal/tmn-main.py
@@ -3,18 +3,69 @@
 #
 
 import requests
+import subprocess
+import re
+import os
+
+def get_mac_info():
+	mac = "11:22:33:44:55"
+	def get_if():
+		try:
+			p = subprocess.Popen((["cat", "/proc/net/dev"]), stdout = subprocess.PIPE)
+			lines = p.stdout.readlines()	
+
+		except Exception, e:
+			print 'get net info failure', str(e)
+			return None
+		
+		netif = None
+		for l in lines:
+			if l.find(':') != -1:
+				xx = l.split(':')[0].strip()
+				if xx == 'lo' or re.search('^w',xx):
+					continue
+				netif = xx
+				break
+
+		return netif
+
+	try:
+		netif = get_if()
+		if not netif:
+			return mac
+
+		p = subprocess.Popen((["ifconfig", netif]), stdout = subprocess.PIPE)
+		lines = p.stdout.readlines()	
+
+	except Exception, e:
+		return mac
+
+	for l in lines:
+		xx = l.strip()
+
+		if xx.startswith('ether'):
+			reret = re.search(r"ether (([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) ", xx)
+			mac = reret.group(1)
+
+		if xx.find("HWaddr") > 0:
+			reret = re.match(r'(.*)(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2})$', xx)
+			mac = reret.group(2)				
+
+	return mac
+
 def user_login(ip = None):
 	ip = "192.168.100.234"
 	username = "test"
 	password = "123456"
+	mac = get_mac_info()
 	try:
 		post_url = 'https://' + ip + '/mng/terminal/login'
 		post_data = {
 			'username' : username,
 			'password' : password,
+			'mac'      : mac,
 		}
 
-		print post_url
 		ret = requests.post(post_url, data=post_data, verify=False, timeout=30)
 
 		print ret
--- mng/manager/handlers/terminal.py
+++ mng/manager/handlers/terminal.py
@@ -2,25 +2,138 @@
 #
 #
 import logging
+import time
+from tornado.ioloop import IOLoop,PeriodicCallback
+
 
 from lib.handler import APIHandler
 
 logger_terminal = logging.getLogger("terminal_log")
 
+
+class LOGON_USER_INFO(object):
+	def __init__(self):
+		super(LOGON_USER_INFO, self).__init__()
+		self.logon_user_info = {}
+
+	def check_heart_beat(self):
+		user_tmps = {}
+		for u_name, info in self.logon_user_info.iteritems():
+			if time.time()  - (info['time']) <= 90:
+				print("below 90...............................")
+				user_tmps[u_name] = info
+
+		print("after user_tmps ====-=======-========== %s" % str(user_tmps))
+		self.logon_user_info = user_tmps
+
+	def add_logon_user_info(self, username=None, client_addr=None, name=None, mac=None):
+		print('%s ---in---add_logon_user_info --------------  %s' % (username, str(self.logon_user_info)))
+		try:
+			for u_name, info in self.logon_user_info.iteritems():
+				print(' info  %s' % (str(info)))
+				if info['name'] == name and info['domain'] == domain and info['mac'] != mac:
+					self.logon_user_info[u_name]['be_out'] = 1
+					
+				if info['name'] == name and info['domain'] == domain and info['addr'] != client_addr:
+					self.logon_user_info[u_name]['be_out'] = 1
+
+
+					
+		except Exception, e:
+			print(' info  %s' % (str(info)))  
+
+		self.logon_user_info[username] = {
+			"name"        : name,
+			"addr"        : client_addr,
+			"time"        : time.time(),
+			"be_out"      : 0,
+			"mac"         : mac,
+			"retry"		  : 0
+		}
+
+		print('%s--after -in---add_logon_user_info -===========  %s' % (username, str(self.logon_user_info)))
+
+		return
+
+	#用户的retry不超过max_count时返回True
+	def can_retry(self, username, max_count):
+		try:
+			if self.logon_user_info[username]["retry"] < max_count:
+				self.logon_user_info[username]["retry"] += 1
+				return True
+			else:
+				return False
+		except Exception, e:
+			return False
+
+	def clear_retry(self, username):
+		try:
+			self.logon_user_info[username]["retry"] = 0
+		except Exception, e:
+			return
+
+
+LOGONER =  LOGON_USER_INFO()
+
+heart_beat = PeriodicCallback(LOGONER.check_heart_beat, 10*1000)
+heart_beat.start()
+
 class UserLoginHandler(APIHandler):
 	def get(self):
 		pass
 
+	#终端用户登录
 	def post(self):
 		try:
 			username = self.get_argument('username', None)
 			password = self.get_argument('password', None)
+			mac = self.get_argument('mac', None)
+
+			terminal_ip = self.request.remote_ip			
 			print "1------"
 			print username
 			print password
+			print terminal_ip
 			print "2------"
+			u_name = username + ':' + mac
+			LOGONER.add_logon_user_info(u_name.upper(), terminal_ip, username.upper(), mac)
 
 		except Exception,e:
 			logger_terminal.info("err = %s" %str(e))
 
-handlers = [(r"/terminal/login", UserLoginHandler)]
+
+class LoginUserHeartBeatHandler(APIHandler):
+	
+	def post(self):
+		try:
+			username = self.get_argument('username', None)
+			mac = self.get_argument('mac', '0')
+			#client_addr = self.request.remote_ip
+				
+			username = username + ':' + mac
+
+			s = {}
+
+			key_name = username.upper()
+			if LOGONER.logon_user_info.has_key(key_name) :
+
+				if LOGONER.logon_user_info[key_name]['be_out'] == 1:
+					s['ret'] = 1002
+					#s['ret'] = 1000
+					LOGONER.logon_user_info.pop(key_name)
+				else:
+					s['ret'] = 1000
+
+					LOGONER.logon_user_info[key_name]["time"] = time.time()
+
+
+			else:
+				s['ret'] = 1001
+
+			self.finish(s)
+
+		except Exception,e:
+			logger_terminal.info("err = %s" %str(e))			
+
+handlers = [(r"/terminal/login", UserLoginHandler),
+			(r"/terminal/heart_beat", LoginUserHeartBeatHandler)]
