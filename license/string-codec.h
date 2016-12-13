3c4be0289ccbc2428a1d412ada09aeb1d9476880
ioriiod0 <ioriiod0@gmail.com>
2016-12-13 11:10:41 +0800

    Modify the makefile of license

--- license/string-codec.h
+++ license/string-codec.h
@@ -1,2 +1,16 @@
+#ifndef __STRINGCODEC_H_
+#define __STRINGCODEC_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 int encode(char *src, char *str);
 int decode(char *src, char *str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
